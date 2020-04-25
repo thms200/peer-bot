49c046cfe001397e68c2aeb82f68b6e049d29a4d
thms200 <thms0504@gmail.com>
2020-04-25 18:33:09 +0900

    refactor: Add consulting Modal container

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,40 +1,22 @@
 import React, { Fragment, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { FiXSquare } from 'react-icons/fi';
 import ModeModal from './ModeModal';
+import ConsultingModal from './ConsultingModal';
 import ChatButton from '../component/ChatButton';
-import { RootState } from '../reducers';
-import { initailCustomer, initialSocket } from '../actions';
-import styles from './App.module.css';
 
 function App() {
-  const dispatch = useDispatch();
-  const socket = useSelector((state: RootState) => state.socket.socket);
-  const nickname = useSelector((state: RootState) => state.customer.nickname);
   const [isShowModeSelect, setIsShowModeSelect] = useState<boolean>(true);
   const [isShowConsulting, setIsShowConsulting] = useState<boolean>(true);
 
   const toggleSelectMode = () => setIsShowModeSelect(!isShowModeSelect);
   const toggleConsulting = () => setIsShowConsulting(!isShowConsulting);
 
-  const disconnectServer = () => {
-    toggleConsulting();
-    const consultant = localStorage.getItem('consultant');
-    socket!.emit('disconnection', nickname, consultant, (message: string) => {
-      alert(message);
-    });
-    dispatch(initailCustomer());
-    socket!.disconnect();
-    dispatch(initialSocket());
-  };
-
   return (
     <Fragment>
-      {!isShowConsulting && <div className={styles.consultingWrapper}>
-        <button onClick={disconnectServer} className={styles.closeButton}>
-          <FiXSquare size={20} color={'white'} />
-        </button>
-      </div>}
+      {!isShowConsulting &&
+        <ConsultingModal
+          onToggleConsulting={toggleConsulting}
+        />
+      }
 
       {!isShowModeSelect &&
         <ModeModal
---
+++ src/containers/ConsultingModal.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { FiXSquare } from 'react-icons/fi';
+import { RootState } from '../reducers';
+import { initailCustomer, initialSocket } from '../actions';
+import styles from './App.module.css';
+
+interface ConsultingProps {
+  onToggleConsulting: () => void;
+}
+
+function ConsultingModal({ onToggleConsulting }: ConsultingProps) {
+  const dispatch = useDispatch();
+  const socket = useSelector((state: RootState) => state.socket.socket);
+  const nickname = useSelector((state: RootState) => state.customer.nickname);
+
+  const disconnectServer = () => {
+    onToggleConsulting();
+    const consultant = localStorage.getItem('consultant');
+    socket!.emit('disconnection', nickname, consultant, (message: string) => {
+      alert(message);
+      dispatch(initailCustomer());
+      socket!.disconnect();
+      dispatch(initialSocket());
+    });
+  };
+
+  return (
+    <div className={styles.consultingWrapper}>
+      <button onClick={disconnectServer} className={styles.closeButton}>
+        <FiXSquare size={20} color={'white'} />
+      </button>
+    </div>
+  );
+}
+
+export default ConsultingModal;
