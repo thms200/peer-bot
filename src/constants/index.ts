0e9d9cc8622511ab94498d40f8005f8f9e213982
thms200 <thms0504@gmail.com>
2020-04-28 13:34:54 +0900

    refactor: Add alert message string -> constant string

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -14,3 +14,9 @@ export const MODE = {
   CAMERA: 'Camera',
   VOICE: 'Voice',
 };
+
+export const ALERT = {
+  VALID_NICKNAME: '사용할 수 있는 닉네임입니다.',
+  INVALID_NICKNAME: '닉네임과 이메일을 입력하시고, 사용가능한지 확인해주세요!',
+  REQUEST_PERMISSION: '카메라, 마이크 접근 권한을 허락해주세요!',
+};
--- src/containers/ModeModal.tsx
+++ src/containers/ModeModal.tsx
@@ -16,7 +16,7 @@ import {
   connectConsultantStream,
   getPeer,
 } from '../actions';
-import { CN, MODE } from '../constants';
+import { CN, MODE, ALERT } from '../constants';
 import styles from './App.module.css';
 const cx = classNames.bind(styles);
 
@@ -33,7 +33,7 @@ function ModeModal({ onToggleSelectMode, onToggleConsulting }: ModeModalProps) {
   let isFirstSignal = true;
 
   const requestConsulting = async(e: React.MouseEvent<HTMLButtonElement>) => {
-    if (!isRequest) return alert('닉네임과 이메일을 입력하시고, 사용가능한지 확인해주세요!');
+    if (!isRequest) return alert(ALERT.INVALID_NICKNAME);
     const mode: string = e.currentTarget.value;
     const isVoice = mode === 'Voice';
     let customerStream: MediaStream;
@@ -65,7 +65,7 @@ function ModeModal({ onToggleSelectMode, onToggleConsulting }: ModeModalProps) {
       dispatch(getPeer(peer));
       dispatch(getCustomerMode(mode));
     } catch(err) {
-      alert('카메라, 마이크 접근 권한을 허락해주세요!');
+      alert(ALERT.REQUEST_PERMISSION);
     }
   };
 
@@ -85,7 +85,7 @@ function ModeModal({ onToggleSelectMode, onToggleConsulting }: ModeModalProps) {
           if (result === 'ok') {
             dispatch(getCustomerName(nickname));
             dispatch(setRequet(true));
-            return alert('사용할 수 있는 닉네임입니다');
+            return alert(ALERT.VALID_NICKNAME);
           }
         });
     } catch(err) {
