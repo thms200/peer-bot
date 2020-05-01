92e15a0d93d38218aaf976fafad7e6aef2c64079
thms200 <48754671+thms200@users.noreply.github.com>
2020-05-02 02:05:47 +0900

    chore: rebuild to fix error (#8)
    * refactor: Fix err (css style in CDN), Modify webpack config
    
    * chore: rebuild to fix css and uglify webpack plugin error

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,10 @@ const glob = require('glob');
 
 module.exports = {
   entry: {
-    'bundle.js': glob.sync('build/static/?(js|css)/main.*.?(js|css)').map(f => path.resolve(__dirname, f)),
+    'bundle.js': glob.sync('build/static/?(js|css)/*.?(js|css)').map(f => path.resolve(__dirname, f)),
   },
   output: {
-    filename: 'build/static/js/bundle.min.js',
+    filename: 'bundle.min.js',
   },
   module: {
     rules: [
