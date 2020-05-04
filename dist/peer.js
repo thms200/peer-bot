ba6820f044f78188a35646cf1d7c254d4694f853
thms200 <thms0504@gmail.com>
2020-05-04 21:07:33 +0900

    refactor: Modify peer.js (for simple cdn code)

--- dist/peer.js
+++ dist/peer.js
@@ -12,4 +12,23 @@
   localStorage.setItem('consultantName', consultantName.id);
   consultantId.remove();
   consultantName.remove();
+
+  //cdn
+  const cdnUrl = 'https://cdn.jsdelivr.net';
+  const github = 'gh/thms200/peer-bot/dist';
+  const mainHashChuck = `${github}/main.67389550.chunk.js`;
+  const mainStyleHaschChuck = `${github}/main.99441111.chunk.css`;
+  const numberHashChuck = `${github}/2.bd7b69aa.chunk.js`;
+  const runtimeMainHash = `${github}/runtime-main.21811c53.js`;
+
+  const head = document.querySelector('head');
+  const link = document.createElement('link');
+  link.setAttribute('rel', 'stylesheet');
+  link.setAttribute('type', 'text/css');
+  link.setAttribute('href', `${cdnUrl}/${mainStyleHaschChuck}`);
+  head.appendChild(link);
+
+  const script = document.createElement('script');
+  script.setAttribute('src', `${cdnUrl}/combine/${mainHashChuck},${numberHashChuck},${runtimeMainHash}`);
+  body.appendChild(script);
 })();
