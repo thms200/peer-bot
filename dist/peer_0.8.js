752e3555c586445a76e3fe2d9f8a4acf841d5552
thms200 <thms0504@gmail.com>
2020-05-08 13:53:34 +0900

    chore: rebuild(handling error)

--- dist/peer_0.8.js
+++ dist/peer_0.8.js
@@ -16,7 +16,7 @@
   //cdn
   const cdnUrl = 'https://cdn.jsdelivr.net';
   const github = 'gh/thms200/peer-bot/dist';
-  const mainHashChuck = `${github}/main.42a1232a.chunk.js`;
+  const mainHashChuck = `${github}/main.6a3aedf8.chunk.js`;
   const mainStyleHaschChuck = `${github}/main.99441111.chunk.css`;
   const numberHashChuck = `${github}/2.36992bba.chunk.js`;
   const runtimeMainHash = `${github}/runtime-main.21811c53.js`;
